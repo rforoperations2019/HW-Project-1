f77d84fa03bd15fc2f4e51f63606f77cd4f0c5ab
Ahmed <ahsanda93@gmail.com>
2019-09-21 09:27:19 -0400

    Expanded template

--- EnergyCrawler/app.R
+++ EnergyCrawler/app.R
@@ -5,15 +5,31 @@ library(ggplot2)
 
 ui <- dashboardPage(
     dashboardHeader(title = "Basic dashboard"),
-    dashboardSidebar(),
+    ## Sidebar content
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+            menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+        )
+    ),
+    ## Body content
     dashboardBody(
-        # Boxes need to be put in a row (or column)
-        fluidRow(
-            box(plotlyOutput("plot1", height = 250)),
+        tabItems(
+            # First tab content
+            tabItem(tabName = "dashboard",
+                    fluidRow(
+                        box(plotlyOutput("plot1", height = 250)),
+                        
+                        box(
+                            title = "Controls",
+                            sliderInput("slider", "Number of observations:", 1, 100, 50)
+                        )
+                    )
+            ),
             
-            box(
-                title = "Controls",
-                sliderInput("slider", "Number of observations:", 1, 100, 50)
+            # Second tab content
+            tabItem(tabName = "widgets",
+                    h2("Widgets tab content")
             )
         )
     )
